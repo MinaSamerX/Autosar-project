d06075b2272fcf4f6830b27b24e7229b67431a7c
Mina Samer <163753387+MinaSamerX@users.noreply.github.com>
2024-10-09 20:58:06 +0300

    Add files via upload

---
+++ AUTOSAR_Project/Debug/Port.d
@@ -0,0 +1,27 @@
+# FIXED
+
+Port.obj: ../Port.c
+Port.obj: ../Port.h
+Port.obj: ../Std_Types.h
+Port.obj: ../Platform_Types.h
+Port.obj: ../Compiler.h
+Port.obj: ../PORT_Cfg.h
+Port.obj: ../Common_Macros.h
+Port.obj: ../Port_Regs.h
+
+../Port.c:
+
+../Port.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../PORT_Cfg.h:
+
+../Common_Macros.h:
+
+../Port_Regs.h:
+
---
+++ AUTOSAR_Project/Debug/Port_PBcfg.d
@@ -0,0 +1,24 @@
+# FIXED
+
+Port_PBcfg.obj: ../Port_PBcfg.c
+Port_PBcfg.obj: ../Port.h
+Port_PBcfg.obj: ../Std_Types.h
+Port_PBcfg.obj: ../Platform_Types.h
+Port_PBcfg.obj: ../Compiler.h
+Port_PBcfg.obj: ../PORT_Cfg.h
+Port_PBcfg.obj: ../Common_Macros.h
+
+../Port_PBcfg.c:
+
+../Port.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../PORT_Cfg.h:
+
+../Common_Macros.h:
+
